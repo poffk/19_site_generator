61c3adcbc93dd8220a23d8cc2ddf4853a9719841
Alexander <Alexander>
2017-01-31 22:44:05 +0300

    Убрана модификация входящего аргумента

--- site_generator.py
+++ site_generator.py
@@ -22,14 +22,18 @@ def read_from_file(filepath):
         return data_file.read()
 
 
-def config_content_ext_change(config):
-    new_config = config
-    for topic in new_config['topics']:
-        for article in new_config['articles']:
+def get_article_config(config):
+    article_config = []
+    for topic in config['topics']:
+        for article in config['articles']:
             if topic['slug'] == article['topic']:
                 original_root, original_ext = os.path.splitext(article['source'])
-                article['source'] = '{}.html'.format(original_root)
-    return new_config
+                article_config.append({
+                    'topic': article['topic'],
+                    'source': '{}.html'.format(original_root),
+                    'title': article['title']
+                })
+    return article_config
 
 
 def load_article(path):
@@ -49,11 +53,11 @@ def directory_create(path):
         os.makedirs(os.path.split(path)[0])
 
 
-def compile_page(config, path_to_template, path_to_save):
+def compile_page(config, article_config, path_to_template, path_to_save):
     html_page = read_from_file(path_to_template)
     template = jinja2.Template(html_page)
     with open(path_to_save, 'w', encoding='utf-8') as f:
-        f.write(template.render(info=config))
+        f.write(template.render(info=config, article_info=article_config))
 
 
 def compile_article(config, path_to_template):
@@ -65,12 +69,12 @@ def compile_article(config, path_to_template):
                 article_text = load_article(os.path.join('articles', article['source']))
                 article_content = {'title': article['title'],
                                    'content': article_text}
-                compile_page(article_content, path_to_template, path_to_save)
+                compile_page(article_content, None, path_to_template, path_to_save)
 
 
 if __name__ == '__main__':
     parser = argparser()
     all_articles_information = load_data(parser.path_to_config)
     compile_article(all_articles_information, 'templates/article.html')
-    all_articles_information_formatted = config_content_ext_change(all_articles_information)
-    compile_page(all_articles_information_formatted, 'templates/index.html', 'pages/index.html')
+    article_information = get_article_config(all_articles_information)
+    compile_page(all_articles_information, article_information, 'templates/index.html', 'pages/index.html')
